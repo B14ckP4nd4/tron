88b188a1638daefa9348173a3a6be898bbfef735
B14ckP4nd4 <B14ckP4nd4@protonmail.com>
2019-12-16 09:58:45 +0100

    Fix Composer Version

--- src/Tron.php
+++ src/Tron.php
@@ -69,6 +69,21 @@
             return true;
         }
 
+        public function generateEncryptedWallet()
+        {
+            $wallet = $this->walletGenerator->generateWallet();
+
+            if($wallet)
+            {
+                $encrypt = SSLEncryption::publicEncrypt($wallet->privateKey);
+                $wallet->privateKey = $encrypt;
+
+                return $wallet;
+            }
+
+            return false;
+        }
+
 
 
 
